c36309d1041121a773dd74095cfddbcd597b2e66
Slava Mednikov <gektorgrom@gmail.com>
2024-12-16 09:49:02 -0700

    Add Anthropic models via Bedrock

--- lib/ai/index.ts
+++ lib/ai/index.ts
@@ -1,11 +1,20 @@
 import { openai } from '@ai-sdk/openai';
+import { bedrock } from '@ai-sdk/amazon-bedrock';
+
 import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
 
 import { customMiddleware } from './custom-middleware';
 
+function getModel(apiIdentifier: string) {
+  if (apiIdentifier.startsWith('anthropic')) {
+    return bedrock(apiIdentifier);
+  }
+  return openai(apiIdentifier);
+}
+
 export const customModel = (apiIdentifier: string) => {
   return wrapLanguageModel({
-    model: openai(apiIdentifier),
+    model: getModel(apiIdentifier),
     middleware: customMiddleware,
   });
 };
--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -9,15 +9,27 @@ export interface Model {
 
 export const models: Array<Model> = [
   {
+    id: 'gpt-4o',
+    label: 'GPT 4o',
+    apiIdentifier: 'gpt-4o',
+    description: 'For complex, multi-step tasks',
+  },
+  {
     id: 'gpt-4o-mini',
     label: 'GPT 4o mini',
     apiIdentifier: 'gpt-4o-mini',
     description: 'Small model for fast, lightweight tasks',
   },
   {
-    id: 'gpt-4o',
-    label: 'GPT 4o',
-    apiIdentifier: 'gpt-4o',
+    id: 'anthropic.claude-3-5-haiku-20241022-v1:0',
+    label: 'Haiku 3.5',
+    apiIdentifier: 'anthropic.claude-3-5-haiku-20241022-v1:0',
+    description: 'For complex, multi-step tasks',
+  },
+  {
+    id: 'anthropic.claude-3-5-sonnet-20241022-v2:0',
+    label: 'Sonnet 3.5',
+    apiIdentifier: 'anthropic.claude-3-5-sonnet-20241022-v2:0',
     description: 'For complex, multi-step tasks',
   },
 ] as const;
